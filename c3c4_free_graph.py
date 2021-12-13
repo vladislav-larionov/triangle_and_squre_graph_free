c028a3892503d52d0941465a30637c4457aa8aa5
Larionov Vladislav <vladislav.larionov@fruct.org>
2021-12-14 00:29:44 +0300

    Add rebuild stage

--- c3c4_free_graph.py
+++ c3c4_free_graph.py
@@ -1,12 +1,15 @@
+import random
 from sys import argv
 
-from graph_reader import read_matrix, matrix_to_edge_list
 from graph import Graph
-from print_utils import print_matrix, print_result, print_result_to_file
+from graph_reader import read_matrix, matrix_to_edge_list
+from print_utils import print_result, print_result_to_file
 from spanning_tree import find_spanning_tree
 from timer_util import timeit
 
-
+"""
+Брать рандомное ребро
+"""
 def find_c3c4_free_graph(graph):
     n = len(graph)
     max_spanning_tree = find_spanning_tree(graph, n)
@@ -14,17 +17,41 @@ def find_c3c4_free_graph(graph):
     for edge in sorted(edges, key=lambda e: e[2], reverse=True):
         if max_spanning_tree.distance(edge[0], edge[1]) >= 4:
             max_spanning_tree.add_edge(edge)
+    old_res = max_spanning_tree.weight
+    print(old_res)
+    print_result_to_file(graph, max_spanning_tree)
+    for i in range(2):
+        max_spanning_tree = rebuild_graph(graph, max_spanning_tree)
+        if old_res == max_spanning_tree.weight:
+            break
+        print_result_to_file(graph, max_spanning_tree)
     return max_spanning_tree
+
+
+def remove_leaves(graph: Graph):
+    leaf_edges, leaves = graph.get_leaves()
+    for leaf in leaf_edges:
+        graph.remove_edge(leaf)
+    return set(leaves), set(leaf_edges)
+
+
+def rebuild_graph(original_graph, graph: Graph):
+    leaves, leaf_edges = remove_leaves(graph)
+    edges = set(matrix_to_edge_list(original_graph)).difference(graph.edges).difference(leaf_edges)
+    for edge in sorted(filter(lambda e: e[0] in leaves or e[1] in leaves, edges), key=lambda e: e[2], reverse=True):
+        dist = graph.distance(edge[0], edge[1])
+        if dist >= 4 or dist == 0:
+            graph.add_edge(edge)
+    print(graph.weight)
+    return graph
 
 
 @timeit
 def main():
     graph = read_matrix(argv[1])
     res = find_c3c4_free_graph(graph)
-    # print_matrix(res.adj_matrix)
-    # print_result(graph, res)
-    print_result_to_file(graph, res, rewrite=True)
-    print()
+    print_result(graph, res, short=True)
+    print_result_to_file(graph, res, rewrite=False, matrix_print=False)
 
 
 if __name__ == "__main__":
--- c3c4_free_graph_via_edge_removing.py
+++ c3c4_free_graph_via_edge_removing.py
@@ -36,9 +36,12 @@ def find_triangles(g):
                     cycles.append([a, b, c])
     return cycles
 
-
+"""
+Хранить вес квадрата
+"""
 def find_squares(g):
     cycles = set()
+    cycles_stringifyred = set()
     n = len(g)
     for a in range(0, n):
         for b in range(0, n):
@@ -46,7 +49,12 @@ def find_squares(g):
                 for d in range(0, n):
                     if g[a][b] and g[a][c] and g[c][d] and g[b][d]:
                         if len({a, b, c, d}) == 4:
+                            # cycles.add((a, b, c, d, g[a][b] + g[a][c] + g[c][d] + g[b][d]))
                             cycles.add((a, b, c, d))
+                            # cycle = tuple(sorted([a, b, c, d]))
+                            # if cycle not in cycles_stringifyred:
+                            #     cycles_stringifyred.add(cycle)
+                            #     cycles.add((a, b, c, d))
     return cycles
 
 
@@ -74,6 +82,7 @@ def remove_squares(matrix, squares):
     b = 1
     c = 2
     d = 3
+    # for square in sorted(squares, key=lambda s: s[4]):
     for square in squares:
         # print(square)
         weights = {matrix[square[a]][square[b]]: (square[a], square[b]),
@@ -86,22 +95,24 @@ def remove_squares(matrix, squares):
         matrix[indexes[1]][indexes[0]] = 0
 
 
-def print_result(matrix, edges):
+def print_result(matrix, edges, short=False):
     print(f'c Вес подграфа = {sum(map(lambda e: e[2], edges))}')
     print(f'p edge {len(matrix)} {len(edges)}')
-    for e in sorted([f"e {edge_to_str(edge)}\n" for edge in edges]):
-        print(e, end='')
+    if not short:
+        for e in sorted([f"e {edge_to_str(edge)}\n" for edge in edges]):
+            print(e, end='')
 
 
 if __name__ == "__main__":
     # matrix = read_matrix('../Taxicab_64_matrix_t.txt')
-    matrix = read_matrix('D:\Projects\Python\С3C4Free\Taxicab_128_matrix.txt')
+    matrix = read_matrix('D:\Projects\Python\С3C4Free\Taxicab_64_matrix.txt')
     remove_triangles(matrix, find_triangles(matrix))
     remove_squares(matrix, find_squares(matrix))
-    # print_result(matrix, matrix_to_edge_list(matrix))
+    print_result(matrix, matrix_to_edge_list(matrix))
     print_result_matrix(matrix, matrix_to_edge_list(matrix))
-
-    matrix = read_matrix('D:\Projects\Python\С3C4Free\Taxicab_128_matrix.txt')
+    #
+    matrix = read_matrix('D:\Projects\Python\С3C4Free\Taxicab_64_matrix.txt')
     remove_squares(matrix, find_squares(matrix))
     remove_triangles(matrix, find_triangles(matrix))
+    print_result(matrix, matrix_to_edge_list(matrix))
     print_result_matrix(matrix, matrix_to_edge_list(matrix))
